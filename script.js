1dacfefe6e0165e50d79f96e7fa4abd2a5d6dc62
holger <losegehtz@gmail.com>
2023-12-16 15:27:51 +0100

    init

---
+++ script.js
@@ -0,0 +1,28 @@
+var navBurger = document.getElementById("navBurger");
+var navItems = document.getElementById("navItems");
+var page = document.getElementById("page");
+var startPage = document.getElementById("startPage");
+
+var x ='';
+
+//navBurger.addEventListener("click", myFunction());
+
+function myFunction(x) {
+  navBurger.classList.toggle("change");
+  navItems.classList.toggle('showHide');
+  startPage.classList.toggle('showHide');
+}
+
+function callPage(page) {
+Array.from(document.querySelectorAll('.page')).forEach(
+  (el) => el.classList.remove('showHide')
+);
+  current = document.getElementById(page+"Page").classList.toggle('showHide');
+  console.log(page);
+  if (page =="start") {
+    myFunction(start);
+    startPage.classList.toggle('showHide');
+    }
+}
+
+
